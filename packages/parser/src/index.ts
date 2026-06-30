30f1e0fa71e36c8c2b703327620075a59445bbe2
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-06-30 18:58:36 -0300

    feat(parser): export migration report builder

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -3,3 +3,5 @@ export * from './dfm/parseDfm.js';
 export * from './dfm/toGestorForm.js';
 export * from './pas/types.js';
 export * from './pas/parsePas.js';
+export * from './migration/types.js';
+export * from './migration/createMigrationModuleReport.js';
