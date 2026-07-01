c9ead45c98c0466f5bf6964745f5223c3624f27e
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-01 18:26:39 -0300

    feat(parser): export resolved form API

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -3,4 +3,6 @@ export * from './dfmParser';
 export * from './dfmToGestorForm';
 export * from './gestorPasEnrichment';
 export * from './pasParser';
+export * from './resolvedForm';
+export * from './resolveForm';
 export * from './types';
