e3c4043ac6102afa955502d4648fd4eb61ffbe15
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-01 16:28:43 -0300

    feat(parser): export Gestor Pascal enrichment

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -1,5 +1,6 @@
 export * from './dfmIntrospection';
 export * from './dfmParser';
 export * from './dfmToGestorForm';
+export * from './gestorPasEnrichment';
 export * from './pasParser';
 export * from './types';
