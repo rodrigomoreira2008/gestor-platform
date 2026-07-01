fb104b148da2424259548ddb4ca9cbc689957a95
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-01 08:48:32 -0300

    feat(frontend): migrate Produto API to CRUD factory

--- apps/frontend/src/modules/produtos/api/produtoApi.ts
+++ apps/frontend/src/modules/produtos/api/produtoApi.ts
@@ -1,27 +1,10 @@
-import { httpClient } from '../../../shared/api/httpClient';
+import { createCrudApi } from '../../../shared/crud/crudApiFactory';
 import type { Produto, ProdutoInput } from '../types/produto';
 
-const basePath = '/api/produtos';
+export const produtoApi = createCrudApi<Produto, ProdutoInput>('/api/produtos');
 
-export async function getProdutos(): Promise<Produto[]> {
-  const response = await httpClient.get<Produto[]>(basePath);
-  return response.data;
-}
-
-export async function getProduto(id: number): Promise<Produto> {
-  const response = await httpClient.get<Produto>(basePath + '/' + id);
-  return response.data;
-}
-
-export async function createProduto(input: ProdutoInput): Promise<Produto> {
-  const response = await httpClient.post<Produto>(basePath, input);
-  return response.data;
-}
-
-export async function updateProduto(id: number, input: ProdutoInput): Promise<void> {
-  await httpClient.put(basePath + '/' + id, input);
-}
-
-export async function removeProduto(id: number): Promise<void> {
-  await httpClient.delete(basePath + '/' + id);
-}
+export const getProdutos = produtoApi.getAll;
+export const getProduto = produtoApi.getById;
+export const createProduto = produtoApi.create;
+export const updateProduto = produtoApi.update;
+export const removeProduto = produtoApi.remove;
