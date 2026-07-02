61560719050cb7bdfb8e4c07b81a558e9875882e
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-02 10:13:24 -0300

    feat(generator): export frontend filter component renderer

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -2,6 +2,7 @@ export * from './backendGenerator';
 export * from './dfmIntrospection';
 export * from './dfmParser';
 export * from './dfmToGestorForm';
+export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
 export * from './frontendGenerator';
 export * from './gestorPasEnrichment';
