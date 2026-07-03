10248d58269362f4bd5a012ecf1f8241a51791fe
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-03 09:51:57 -0300

    feat(generator): export frontend tab generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -8,6 +8,7 @@ export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
 export * from './frontendGenerator';
 export * from './frontendLookupGenerator';
+export * from './frontendTabGenerator';
 export * from './gestorPasEnrichment';
 export * from './lookupInference';
 export * from './migrationReportGenerator';
