dda621c6d7af807aafbf5c45669c9cfa8205fee4
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-16 16:18:49 -0300

    test(delphi-parser): include navigation snippets in advanced frontend suite

--- packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
+++ packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
@@ -24,6 +24,7 @@ const fixtures: FixtureCase[] = [
 
 const validators: ValidatorCase[] = [
   { name: 'types', file: 'src/cli/validateGeneratedFrontendTypes.ts' },
+  { name: 'navigation', file: 'src/cli/validateGeneratedFrontendNavigation.ts' },
   { name: 'tabbed-form', file: 'src/cli/validateGeneratedFrontendTabbedForm.ts' },
   { name: 'tabs', file: 'src/cli/validateGeneratedFrontendTabs.ts' },
   { name: 'filters', file: 'src/cli/validateGeneratedFrontendFilters.ts' },
