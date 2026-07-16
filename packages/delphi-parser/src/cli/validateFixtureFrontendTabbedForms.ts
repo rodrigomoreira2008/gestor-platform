1dbb3a21b30b47bc98e02ad7b716c00f1724e04e
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-16 15:19:18 -0300

    test(delphi-parser): include frontend tabs in advanced fixture suite

--- packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
+++ packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
@@ -24,6 +24,7 @@ const fixtures: FixtureCase[] = [
 
 const validators: ValidatorCase[] = [
   { name: 'tabbed-form', file: 'src/cli/validateGeneratedFrontendTabbedForm.ts' },
+  { name: 'tabs', file: 'src/cli/validateGeneratedFrontendTabs.ts' },
   { name: 'filters', file: 'src/cli/validateGeneratedFrontendFilters.ts' },
   { name: 'detail-grids', file: 'src/cli/validateGeneratedFrontendDetailGrids.ts' },
   { name: 'detail-hooks', file: 'src/cli/validateGeneratedFrontendDetailHooks.ts' },
