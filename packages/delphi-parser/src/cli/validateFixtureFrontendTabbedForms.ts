69dc15d28733ad2cd324b3d6a5d446537441bf8b
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-16 16:31:23 -0300

    test(delphi-parser): include frontend manifest contract

--- packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
+++ packages/delphi-parser/src/cli/validateFixtureFrontendTabbedForms.ts
@@ -23,6 +23,7 @@ const fixtures: FixtureCase[] = [
 ];
 
 const validators: ValidatorCase[] = [
+  { name: 'manifest', file: 'src/cli/validateGeneratedFrontendManifest.ts' },
   { name: 'types', file: 'src/cli/validateGeneratedFrontendTypes.ts' },
   { name: 'navigation', file: 'src/cli/validateGeneratedFrontendNavigation.ts' },
   { name: 'tabbed-form', file: 'src/cli/validateGeneratedFrontendTabbedForm.ts' },
