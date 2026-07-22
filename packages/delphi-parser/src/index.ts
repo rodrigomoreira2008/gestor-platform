5a6e4a3f1818402157c27df54830e6e8948a2c89
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 16:58:50 -0300

    feat(delphi-parser): export warning word break generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningLogicalTextAlignGenerator';
+export * from './frontendOfflineCacheWarningWordBreakGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
