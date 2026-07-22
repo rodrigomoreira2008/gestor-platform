edcea664c29b9ee6d924f349cccee02de223af41
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 13:31:34 -0300

    feat(delphi-parser): export offline cache warning language generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningTextHyphenationGenerator';
+export * from './frontendOfflineCacheWarningLanguageGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
