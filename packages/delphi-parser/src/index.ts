7bd9009d8e95b1fc4489fe540e39b1d52c8d8c86
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 09:06:39 -0300

    feat(delphi-parser): export frontend filter reset generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendCsvExportGenerator';
+export * from './frontendFilterResetGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
