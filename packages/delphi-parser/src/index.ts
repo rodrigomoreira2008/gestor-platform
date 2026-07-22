73c515b3d120d7e66272d1ae784dd7694096d566
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 11:33:53 -0300

    feat(delphi-parser): export offline cache warning button role generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningStateDescriptionGenerator';
+export * from './frontendOfflineCacheWarningButtonRoleGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
