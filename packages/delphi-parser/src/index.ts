e1e2bf75153b76a26900050abc83d4980293f640
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 10:09:19 -0300

    feat(delphi-parser): export offline cache warning generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheTimestampGenerator';
+export * from './frontendOfflineCacheWarningGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
