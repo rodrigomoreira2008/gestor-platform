cd1eeee04a6c844be02d8bd0d68c0f35b63c4294
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 13:42:48 -0300

    feat(delphi-parser): export offline cache warning bidi isolation generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningDirectionGenerator';
+export * from './frontendOfflineCacheWarningBidiIsolationGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
