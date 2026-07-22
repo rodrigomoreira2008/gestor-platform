c71d57da2b02f0e2af4ed7941bb9ccf8a8139aaa
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 10:53:22 -0300

    feat(delphi-parser): export offline cache warning Escape shortcut generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningEscapeDismissGenerator';
+export * from './frontendOfflineCacheWarningEscapeShortcutGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
