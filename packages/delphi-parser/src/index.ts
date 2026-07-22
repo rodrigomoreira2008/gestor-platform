9358006573fc3b298b6931b5769e579109c01886
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 09:46:52 -0300

    feat(delphi-parser): export offline error recovery generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendQueryStatusAnnouncementGenerator';
+export * from './frontendOfflineErrorRecoveryGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
