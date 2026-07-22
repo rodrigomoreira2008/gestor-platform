9f6a759e8526cc607c19359645b8fdcf3a230f80
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 12:24:21 -0300

    feat(delphi-parser): export offline warning focus-visible generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningShortcutAvailabilityGenerator';
+export * from './frontendOfflineCacheWarningFocusVisibleGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
