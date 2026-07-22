fd577cb3d86c8270978f2b3c4862f637315054bf
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 12:33:48 -0300

    feat(delphi-parser): export offline warning interaction feedback generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningFocusVisibleGenerator';
+export * from './frontendOfflineCacheWarningInteractionFeedbackGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
