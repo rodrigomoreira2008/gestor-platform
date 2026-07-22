863b04ef6d7bd572e2ad33df30bd56ece5b7d537
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-22 09:43:45 -0300

    feat(delphi-parser): export query status announcement generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineRefreshGenerator';
+export * from './frontendQueryStatusAnnouncementGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
