70f729848c4363dd8364f5c1d63fa26b6027eb08
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 08:27:28 -0300

    feat(delphi-parser): export offline warning font style generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningFontStretchGenerator';
+export * from './frontendOfflineCacheWarningFontStyleGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
