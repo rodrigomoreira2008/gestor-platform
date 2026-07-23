d350f8589cca1f40c0302c4e59aaa01e47d37ab2
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 08:18:20 -0300

    feat(delphi-parser): export offline cache warning font size adjust generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningFontPaletteGenerator';
+export * from './frontendOfflineCacheWarningFontSizeAdjustGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
