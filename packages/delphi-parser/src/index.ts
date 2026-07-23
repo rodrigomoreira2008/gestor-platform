74179a30231f5ad87c5ef9b721d8e9a1aedb1766
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 08:32:43 -0300

    feat(delphi-parser): export offline cache warning font weight generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningFontStyleGenerator';
+export * from './frontendOfflineCacheWarningFontWeightGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
