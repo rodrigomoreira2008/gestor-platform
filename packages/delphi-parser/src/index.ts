5e4686aac8bdd29598501c50df35d617501cce74
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 09:29:25 -0300

    feat(delphi-parser): export offline warning word spacing generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningLetterSpacingGenerator';
+export * from './frontendOfflineCacheWarningWordSpacingGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
