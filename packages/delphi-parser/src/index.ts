f30bf3c4e6ad2374be2a031cf0ef57fbe8be5354
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 11:22:49 -0300

    feat(delphi-parser): export offline cache warning text decoration skip ink generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningTextUnderlineOffsetGenerator';
+export * from './frontendOfflineCacheWarningTextDecorationSkipInkGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
