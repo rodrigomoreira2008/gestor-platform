86d8ace55f543fba0863f8dc0ea2567274e19c34
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 09:56:31 -0300

    feat(delphi-parser): export offline cache warning text shadow generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningTextDecorationGenerator';
+export * from './frontendOfflineCacheWarningTextShadowGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
