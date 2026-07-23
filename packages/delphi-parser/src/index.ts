5eee4a2a2d50d618b8de8d94d7843eb080fb04cd
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 08:02:37 -0300

    feat(delphi-parser): export offline cache warning font palette generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningFontVariationSettingsGenerator';
+export * from './frontendOfflineCacheWarningFontPaletteGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
