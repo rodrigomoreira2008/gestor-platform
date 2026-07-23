ffea404c3840f36acdab5da7d1004ae33026bb78
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-23 11:34:19 -0300

    feat(delphi-parser): export offline cache warning text decoration thickness generator

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -9,7 +9,7 @@ export * from './frontendDetailGridGenerator';
 export * from './frontendDetailHookGenerator';
 export * from './frontendFilterComponentGenerator';
 export * from './frontendFilterGenerator';
-export * from './frontendOfflineCacheWarningTextDecorationSkipInkGenerator';
+export * from './frontendOfflineCacheWarningTextDecorationThicknessGenerator';
 export * from './frontendLookupComponentGenerator';
 export * from './frontendLookupGenerator';
 export * from './frontendSchemaGenerator';
