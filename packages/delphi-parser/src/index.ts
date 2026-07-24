6c38e75594362047e952107a5970d3f92429a880
rodrigomoreira2008 <rodrigomoreira2008@gmail.com>
2026-07-24 08:30:41 -0300

    feat(delphi-parser): export business rule field enrichment

--- packages/delphi-parser/src/index.ts
+++ packages/delphi-parser/src/index.ts
@@ -1,4 +1,5 @@
 export * from './backendGenerator';
+export * from './businessRuleFieldEnrichment';
 export * from './businessRuleInference';
 export * from './componentMapping';
 export * from './databaseInference';
